ad05199c0b4b8bd756ed1fce3d3ad8209257da5b
jordanstholmes <36938897+jordanstholmes@users.noreply.github.com>
2019-06-15 12:39:25 -0700

    Replace obvious parameter name with "data"

--- async-test/writeToFile.js
+++ async-test/writeToFile.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
-const writeToFile = (num, callback) => {
-  fs.writeFile('numFile.txt', num, 'utf8', (err, result) => {
+const writeToFile = (data, callback) => {
+  fs.writeFile('numFile.txt', data, 'utf8', (err) => {
     if (err) {
       console.log(err);
     } else {
-      callback();
+      callback('wrote to file!');
     }
   });
 };
